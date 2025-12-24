b31359dcdd1e6a0f74d09f4306ae362836a546ac
mint-julip <sandysmith@soartn.org>
2025-12-23 23:30:56 -0500

    Update ot.js

--- js/ot.js
+++ js/ot.js
@@ -1,1 +1,24 @@
+function submitOT() {
+  const ticket = "OT-" + Date.now();
+
+  const params = {
+    ticket,
+    hm: hm.value,
+    emp: employee.value,
+    house: house.value,
+    date: date.value,
+    hours: hours.value,
+    reason: reason.value,
+
+    approve_link: `https://YOURUSERNAME.github.io/YOURREPO/ot-approval.html?ticket=${ticket}&decision=approved`,
+    deny_link: `https://YOURUSERNAME.github.io/YOURREPO/ot-approval.html?ticket=${ticket}&decision=denied`
+  };
+
+  emailjs.send("service_soartn", "template_ot_request", params)
+    .then(() => {
+      alert("OT Submitted. Ticket " + ticket);
+      confetti();
+    })
+    .catch(err => console.error(err));
+}
 
