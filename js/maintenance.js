d4baaf9dd0fa0127493e0af0e749e6a188464b00
mint-julip <sandysmith@soartn.org>
2025-12-23 23:37:13 -0500

    Update maintenance.js

--- js/maintenance.js
+++ js/maintenance.js
@@ -1,1 +1,22 @@
+function submitMaintenance() {
+  const ticket = "MT-" + Date.now();
+
+  const params = {
+    ticket,
+    name: name.value,
+    contact: contact.value,
+    house: house.value,
+    work: work.value,
+    expected: expected.value,
+    to_email: "soarhr@soartn.org",
+    cc_email: "kobypresley@soartn.org,cherylhintz@soartn.org,alishasanders@soartn.org"
+  };
+
+  emailjs.send("service_soartn", "template_maintenance_request", params)
+    .then(() => {
+      alert("Maintenance Request Submitted. Ticket " + ticket);
+      confetti();
+    })
+    .catch(err => console.error(err));
+}
 
