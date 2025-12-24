47cccc2a842457365af7966330bfd7ea150fa0ab
mint-julip <sandysmith@soartn.org>
2025-12-23 23:38:11 -0500

    Update expense.js

--- js/expense.js
+++ js/expense.js
@@ -1,1 +1,21 @@
+function submitExpense() {
+  const ticket = "EX-" + Date.now();
+
+  const params = {
+    ticket,
+    name: name.value,
+    email: email.value,
+    amount: amount.value,
+    reason: reason.value,
+    to_email: "finance@soartn.org",
+    cc_email: "soarhr@soartn.org"
+  };
+
+  emailjs.send("service_soartn", "template_expense_request", params)
+    .then(() => {
+      alert("Expense Request Submitted. Ticket " + ticket);
+      confetti();
+    })
+    .catch(err => console.error(err));
+}
 
