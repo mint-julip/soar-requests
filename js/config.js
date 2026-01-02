017d2f527f953a2c943cb268c272a669d7973f02
mint-julip <sandysmith@soartn.org>
2026-01-01 22:18:59 -0500

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,14 @@
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycby-a4gm5kpU1ZCBgQJyxkT3Pw5PeIYb63N0ZbnILJZVlCLIz1SxtxsjDV-aKzwGn5oyLA/exec";
+
+const HR_EMAILS = [
+  "soarhr@soartn.org"
+];
+
+const EXPENSE_CC = {
+  default: "marialaulusa@soartn.org",
+  "100": "marialaulusa@soartn.org",
+  "500": "marialaulusa@soartn.org, cherylhintz@soartn.org",
+  "1000": "marialaulusa@soartn.org, cherylhintz@soartn.org, nikkigoodwin@soartn.org"
+};
+
+// Other shared constants
