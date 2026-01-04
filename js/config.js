c04e5791c13e13b369f3700f5b8f6b00f089e2f2
mint-julip <sandysmith@soartn.org>
2026-01-03 22:45:57 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,12 @@
+// ============================
+// GLOBAL CONFIG (LOAD ONCE)
+// ============================
+
+// Google Apps Script endpoint
+const GOOGLE_SCRIPT_URL =
+  "https://script.google.com/macros/s/AKfycby-a4gm5kpU1ZCBgQJyxkT3Pw5PeIYb63N0ZbnILJZVlCLIz1SxtxsjDV-aKzwGn5oyLA/exec";
+
 // EmailJS
 const EMAILJS_PUBLIC_KEY = "sLNm5JCzwihAuVon0";
 emailjs.init(EMAILJS_PUBLIC_KEY);
+
