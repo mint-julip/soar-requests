2e4b10dd3d12d17f94e62d8846ce659888efbaaa
mint-julip <sandysmith@soartn.org>
2026-01-04 19:47:23 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,6 +2,13 @@
 // GLOBAL CONFIG (SAFE LOAD)
 // ============================
 
+// config.js
+const EMAILJS_SERVICE_ID = "service_lk56r2m";
+
+const OT_REQUEST_TEMPLATE = "template_78v4e8s";
+const OT_APPROVED_TEMPLATE = "template_f5t0nig";
+const OT_DENIED_TEMPLATE = "template_zyx9oic";
+
 // Google Apps Script endpoint (ONE place only)
 const GOOGLE_SCRIPT_URL =
   "https://script.google.com/macros/s/AKfycby-a4gm5kpU1ZCBgQJyxkT3Pw5PeIYb63N0ZbnILJZVlCLIz1SxtxsjDV-aKzwGn5oyLA/exec";
